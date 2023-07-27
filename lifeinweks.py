e0e6cbd0b04b40900c97f01cf1ddd674a9a160ea
bibha <bibhashkalita94@gmail.com>
2023-07-27 14:18:48 +0530

    Added Comments

--- lifeinweks.py
+++ lifeinweks.py
@@ -3,10 +3,10 @@ age = input("What is your current age? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-new_age = 90 - int(age)
-days = new_age * 365
-weeks = new_age * 52
-months = new_age * 12
+new_age = 90 - int(age) #lets say age 20 , 90 -20 =70
+days = new_age * 365 #days = 70 * 365 = 25,550
+weeks = new_age * 52 #weeks = 70 * 52 = 3640
+months = new_age * 12 #months = 70 x 12 = 840
 print(f"You have {days} days, {weeks} weeks, and {months} months left.")
 
 
