2bb7a701b70ab8c494234234db9ba008eeced679
bibha <bibhashkalita94@gmail.com>
2023-07-31 00:48:03 +0530

    added new file

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercostar!!")
+height = int(input("enter your height in Cm "))
+
+if height >= 120:
+    print("you can ride the rollercoaster")
+else:
+    print("Sorry you need to grow taller")
---
+++ swappingnumbers.py
@@ -0,0 +1,8 @@
+a = input("please enter a number a : ")
+b = input("Please Enter a number b : ")
+
+c = a
+a = b
+b = c
+print(a)
+print(b)
