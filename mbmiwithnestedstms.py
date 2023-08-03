341370ea180856b7ccd87529341c81c6f837623a
bibha <bibhashkalita94@gmail.com>
2023-08-03 17:34:26 +0530

    Bmi with Nested statements

--- mbmiwithnestedstms.py
+++ mbmiwithnestedstms.py
@@ -5,14 +5,14 @@ weight = float(input("enter your weight in kg: "))
 
 #Write your code below this line 👇
 bmi = (weight / (height**2))
-roundbmi = round(bmi)
+roubmi = round(bmi)
 if bmi < 18.5:
-    print("Your BMI is " + str(roundbmi) + ", you are underweight.")
+    print("Your BMI is " + str(roubmi) + ", you are underweight.")
 elif (bmi > 18.5) and (bmi < 25):
-    print("Your BMI is " + str(roundbmi) + ", you have a normal weight.")
+    print("Your BMI is " + str(roubmi) + ", you have a normal weight.")
 elif bmi > 25 or bmi < 30:
-    print("Your BMI is " + str(roundbmi) + ", you are slightly overweight.")
+    print("Your BMI is " + str(roubmi) + ", you are slightly overweight.")
 elif bmi > 30 or bmi < 35:
-    print("Your BMI is " + str(roundbmi) + ", you are obese.")
-else :
-    print("Your BMI is " + str(roundbmi) + ", you are clinically obese.")
+    print("Your BMI is " + str(roubmi) + ", you are obese.")
+else:
+    print("Your BMI is " + str(roubmi) + ", you are clinically obese.\n")
